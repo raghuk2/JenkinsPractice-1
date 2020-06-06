125dbeb63ff5546cb0228a3b2a2e7a0a569590da
raghuk2 <39421611+raghuk2@users.noreply.github.com>
2020-06-06 11:20:42 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,7 +5,7 @@ public class HelloGuys {
 		System.out.println("Hey Agni");
 		System.out.println("Hey Riya");
 		System.out.println("Hey Duck");
-		System.out.println("Hey Arya");//newly added
+		System.out.println("Hey Arya");
 		System.out.println("Hey Aishu");//newly added
 	}
 
